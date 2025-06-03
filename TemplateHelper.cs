83209794f623f1b7d480ea1065b1b7f457f88606
bmpq <73074311+bmpq@users.noreply.github.com>
2025-06-03 20:33:35 +0300

    real loot item spawn in raid

---
+++ TemplateHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Comfort.Common;
+using EFT.InventoryLogic;
+
+#nullable enable
+
+namespace tarkin.moonitem
+{
+    // no idea what is happening in this class, i stole this from someone on github
+    internal class TemplateHelper
+    {
+        private static readonly Dictionary<string, ItemTemplate> _templates = new Dictionary<string, ItemTemplate>();
+
+        private static void UpdateTemplates()
+        {
+            if (!Singleton<ItemFactoryClass>.Instantiated)
+                return;
+
+            var mongoTemplates = Singleton<ItemFactoryClass>
+                .Instance
+                .ItemTemplates;
+
+            if (_templates.Count == mongoTemplates.Count)
+                return;
+
+            foreach (var kv in mongoTemplates)
+            {
+                if (!_templates.ContainsKey(kv.Key.ToString()))
+                {
+                    _templates.Add(kv.Key.ToString(), kv.Value);
+                }
+            }
+        }
+
+        internal static ItemTemplate[] FindTemplates(string searchShortNameOrTemplateId)
+        {
+            UpdateTemplates();
+
+            ItemTemplate? template;
+            if (_templates.TryGetValue(searchShortNameOrTemplateId, out template) && template != null)
+            {
+                return new ItemTemplate[] { template };
+            }
+
+            List<ItemTemplate> foundTemplates = new List<ItemTemplate>();
+            foreach (ItemTemplate t in _templates.Values)
+            {
+                string? shortName = t.ShortNameLocalizationKey?.Localized();
+                string? name = t.NameLocalizationKey?.Localized();
+
+                bool match = false;
+                if (shortName != null && shortName.IndexOf(searchShortNameOrTemplateId, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    match = true;
+                }
+
+                if (!match && name != null && name.IndexOf(searchShortNameOrTemplateId, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    match = true;
+                }
+
+                if (match)
+                {
+                    foundTemplates.Add(t);
+                }
+            }
+
+            return foundTemplates.ToArray();
+        }
+    }
+}
--- TheMoonItemController.cs
+++ TheMoonItemController.cs
@@ -4,33 +4,108 @@ using EFT.InventoryLogic;
 using System.Threading.Tasks;
 using UnityEngine;
 using System.Threading;
+using EFT.Interactive;
+using System;
+using Random = UnityEngine.Random;
+using System.Linq;
+using Diz.Utils;
+using System.Collections.Generic;
+using EFT.CameraControl;
 
 namespace tarkin.moonitem
 {
     internal class TheMoonItemController : MonoBehaviour
     {
+        public static TheMoonItemController Instance
+        { 
+        get
+            {
+                if (_instance == null)
+                    _instance = FindObjectOfType<TheMoonItemController>();
+                return _instance;
+            }
+        }
+        private static TheMoonItemController _instance;
+        Player player => Singleton<GameWorld>.Instance.MainPlayer;
+
         Item item;
-        GameObject itemObject;
+        public GameObject itemObject;
+        LootItem lootItem;
+        bool calledCreateStaticLoot;
+
+        private static readonly Dictionary<string, ItemTemplate> _templates = new Dictionary<string, ItemTemplate>();
+
+
+        internal static void SpawnTemplate(string template, Player player, Func<ItemTemplate, bool> filter)
+        {
+            var result = TemplateHelper.FindTemplates(template).FirstOrDefault();
+
+            if (result == null)
+                return;
+
+            SpawnTemplate(result, player);
+        }
 
         void Start()
         {
-            LoadBundle();
+            SpawnTemplate("683e1aca717050d545879d90", player, null);
         }
 
-        async Task LoadBundle()
+        private static void SpawnTemplate(ItemTemplate template, Player player)
         {
-            // flimsy as fuck
-            DependencyGraphClass<IEasyBundle>.GClass3901 gclass3901_0 = Singleton<IEasyAssets>.Instance.Retain(new string[] { "moon.bundle" }, null, default(CancellationToken));
-            await GClass1628.LoadBundles(gclass3901_0);
-            SpawnItem();
+            var poolManager = Singleton<PoolManagerClass>.Instance;
+
+            poolManager
+                .LoadBundlesAndCreatePools(PoolManagerClass.PoolsCategory.Raid, PoolManagerClass.AssemblyType.Online, template.AllResources.ToArray(), JobPriorityClass.Immediate)
+                .ContinueWith(task =>
+                {
+                    AsyncWorker.RunInMainTread(delegate
+                    {
+                        if (task.IsFaulted)
+                        {
+                            Plugin.Log.LogError("load bundle failed");
+                        }
+                        else
+                        {
+                            var itemFactory = Singleton<ItemFactoryClass>.Instance;
+                            var item = itemFactory.CreateItem(MongoID.Generate(), template._id, null);
+                            if (item == null)
+                            {
+                                Plugin.Log.LogError("failed to create item");
+                            }
+                            else
+                            {
+                                _ = new TraderControllerClass(item, item.Id, item.ShortName);
+                                var go = poolManager.CreateLootPrefab(item, ECameraType.Default);
+
+                                go.SetActive(value: true);
+                                var lootItem = Singleton<GameWorld>.Instance.CreateLootWithRigidbody(go, item, item.ShortName, randomRotation: false, null, out _, true);
+
+                                var transform = player.Transform;
+                                var position = transform.position
+                                               + transform.right * Random.Range(-1f, 1f)
+                                               + transform.forward * 2f
+                                               + transform.up * 0.5f;
+
+                                lootItem.transform.SetPositionAndRotation(position, transform.rotation);
+                                lootItem.LastOwner = player;
+
+                                SetupItem(itemFactory, item, lootItem);
+                            }
+                        }
+                    });
+
+                    return Task.CompletedTask;
+                });
         }
 
-        void SpawnItem()
+        private static void SetupItem(ItemFactoryClass itemFactory, Item item, LootItem lootItem)
         {
-            item = Singleton<ItemFactoryClass>.Instance.CreateItem(MongoID.Generate(true), "683e1aca717050d545879d90", null);
-            itemObject = Singleton<PoolManagerClass>.Instance.CreateItem(item, false);
+            // ugly, wrote wehn i was eepy, todo cleanup
+            TheMoonItemController.Instance.itemObject = lootItem.TrackableTransform.gameObject;
 
-            Singleton<GameWorld>.Instance.CreateStaticLoot(itemObject, item, item.LocalizedName(), true, null);
+            item.SpawnedInSession = true; // found in raid
+
         }
 
         void FixedUpdate()
@@ -43,9 +118,6 @@ namespace tarkin.moonitem
             if (itemObject == null)
                 return;
 
-            itemObject.gameObject.SetActive(true);
-
-            Player player = Singleton<GameWorld>.Instance?.MainPlayer;
             if (player != null)
             {
                 itemObject.transform.position = player.Position + new Vector3(0, 1.6f, 0) + player.Transform.Original.forward * 0.7f;
